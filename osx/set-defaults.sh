483f21e57dc96ceebd394b0d85c873572bb45037
Tom Bell <tomb@tomb.io>
2012-03-31 17:57:08 +0100

    Update OSX defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -18,3 +18,12 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
+
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Show all filename extensions in Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
