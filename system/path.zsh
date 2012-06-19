d26743f11c7f8fa1293273f9ac2786646f087410
Parker Moore <parkrmoore@gmail.com>
2012-06-18 20:20:10 -0400

    Updated PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/scala/bin:/usr/local/mysql/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/Cellar/ruby/1.9.3-p194/bin:/usr/local/Cellar/ruby/1.9.3-p194/lib/ruby/gems/1.9.1/bin:/usr/local/sbin:$ZSH/bin:/usr/local/scala/bin:/usr/local/mysql/bin:$PATH"
