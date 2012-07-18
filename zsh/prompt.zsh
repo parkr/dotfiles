57d09136f1e5c55718ad3d8ecbab6607d29fc6d7
Parker Moore <parkrmoore@gmail.com>
2012-07-18 11:23:51 -0400

    I just wasn't ready for Git and it wasn't ready for me.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -35,7 +35,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%~%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(username) in $(directory_name) $(git_dirty)\n› '
+export PROMPT=$'\n$(username) in $(directory_name)\n$ '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
