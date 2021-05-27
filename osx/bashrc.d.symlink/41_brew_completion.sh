994fadad30a5265fffba9540034d82b62543dc19
Parker Moore <parkrmoore@gmail.com>
2021-05-26 21:25:24 -0400

    Fix git setups

--- osx/bashrc.d.symlink/41_brew_completion.sh
+++ osx/bashrc.d.symlink/41_brew_completion.sh
@@ -1,7 +1,11 @@
 HOMEBREW_PREFIX="/usr/local"
 if [ -d "$HOMEBREW_PREFIX/Cellar" ]; then
+  BASH_COMPLETION_PATH="${HOMEBREW_PREFIX}/etc/bash_completion.d/${file}"
+  if [ -f "$BASH_COMPLETION_PATH" ]; then
+    source_sub_with_bench "$HOME/.bashrc.d/41_brew_completion.sh" "${BASH_COMPLETION_PATH}"
+  fi
   declare -a BREW_COMPLETIONS
-  BREW_COMPLETIONS=(bash brew cheat.bash fd.bash gh git-completion.bash git-prompt.sh rg.bash youtube-dl.bash-completion)
+  BREW_COMPLETIONS=(brew cheat.bash fd.bash gh git-completion.bash git-prompt.sh rg.bash youtube-dl.bash-completion)
   for file in ${BREW_COMPLETIONS[@]}; do
     source_sub_with_bench "$HOME/.bashrc.d/41_brew_completion.sh" "${HOMEBREW_PREFIX}/etc/bash_completion.d/${file}"
   done
