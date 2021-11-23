ee950132a91d496bb7ca5aceb22a89b1c091171d
Parker Moore <parkrmoore@gmail.com>
2021-11-23 10:01:59 -0800

    99_localrc.sh: use ~/.localrc.d

--- osx/bashrc.d.symlink/99_localrc.sh
+++ osx/bashrc.d.symlink/99_localrc.sh
@@ -1,3 +1,9 @@
 if [ -f "$HOME/.localrc" ]; then
   source $HOME/.localrc
 fi
+
+if [ -d "$HOME/.localrc.d" ]; then
+ for i in ~/.localrc.d/*.sh; do
+   source_sub_with_bench "$HOME/.bashrc.d/99_localrc.d" "$i"
+ done; unset i
+fi
