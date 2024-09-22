7e23582e11d404676ed8dcd45101a808f43ba8ff
Parker Moore <parkrmoore@gmail.com>
2024-09-21 17:07:49 -0700

    yt-dlp has replaced youtube-dl due to lack of releases of the latter

--- osx/bashrc.d.symlink/41_brew_completion.sh
+++ osx/bashrc.d.symlink/41_brew_completion.sh
@@ -5,7 +5,7 @@ if [ -d "${HOMEBREW_PREFIX}/Cellar" ]; then
     source_sub_with_bench "$HOME/.bashrc.d/41_brew_completion.sh" "${BASH_COMPLETION_PATH}"
   fi
   declare -a BREW_COMPLETIONS
-  BREW_COMPLETIONS=(brew cheat.bash fd.bash gh git-completion.bash git-prompt.sh rg.bash youtube-dl.bash-completion)
+  BREW_COMPLETIONS=(brew cheat.bash fd.bash gh git-completion.bash git-prompt.sh rg.bash yt-dlp.bash-completion)
   for file in ${BREW_COMPLETIONS[@]}; do
     if [ -f "${HOMEBREW_PREFIX}/etc/bash_completion.d/${file}" ]; then
       source_sub_with_bench "$HOME/.bashrc.d/41_brew_completion.sh" "${HOMEBREW_PREFIX}/etc/bash_completion.d/${file}"
