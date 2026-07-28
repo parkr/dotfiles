394624aa62e9ae2a40ca59d519c9f53eecfd380d
Parker Moore <parkrmoore@gmail.com>
2026-07-28 15:55:24 -0700

    Fix PATH construction so it doesn't mangle

--- osx/bashrc.d.symlink/10_env.sh
+++ osx/bashrc.d.symlink/10_env.sh
@@ -4,23 +4,40 @@ if [ -d "/opt/homebrew" ] && [ ! -f "/usr/local/bin/brew" ]; then
 fi
 
 add_to_path() {
-  if [ -d "$1" ]; then
-    PATH="$1:$PATH"
+  # Only add if it's a directory and NOT already in PATH
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    # If PATH is empty, just set it. Otherwise, prepend with a colon.
+    PATH="$1${PATH:+:$PATH}"
   fi
 }
 
-PATH="/usr/bin:/bin:/usr/sbin:/sbin"
-PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+# Base system paths (added in reverse priority order since add_to_path prepends)
+add_to_path "/sbin"
+add_to_path "/usr/sbin"
+add_to_path "/bin"
+add_to_path "/usr/bin"
+
+# Local user paths
+add_to_path "/usr/local/sbin"
+add_to_path "/usr/local/bin"
+
+# Go, Java, Dotfiles, etc.
 add_to_path "/usr/local/opt/go/libexec/bin"
 add_to_path "$HOME/.dotfiles/bin"
 add_to_path "$HOME/dotfiles/bin"
 add_to_path "$HOME/.bin"
 add_to_path "$HOME/bin"
 add_to_path "$HOME/.local/bin"
-[[ -d "/usr/local/opt/openjdk/bin" ]] && add_to_path "/usr/local/opt/openjdk/bin"
-[[ -d "$HOME/go" ]]  && PATH="$HOME/go/bin:$PATH" && export GOPATH="$HOME/go"
-[[ -d "$HOME/.cargo/bin" ]] && PATH="$HOME/.cargo/bin:$PATH"
-[[ -n "${HOMEBREW_PREFIX}" ]] && PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
+add_to_path "/usr/local/opt/openjdk/bin"
+
+[[ -d "$HOME/go" ]] && add_to_path "$HOME/go/bin" && export GOPATH="$HOME/go"
+
+add_to_path "$HOME/.cargo/bin"
+
+if [[ -n "${HOMEBREW_PREFIX}" ]]; then
+  add_to_path "$HOMEBREW_PREFIX/sbin"
+  add_to_path "$HOMEBREW_PREFIX/bin"
+fi
 
 for pkg in $HOME/.dotfiles/pkg/*; do
   add_to_path "$pkg/bin"
